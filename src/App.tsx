9895c918ee252542f06a541f7bae798ef65275ba
iamLolade <adeniyiotemade@gmail.com>
2022-02-22 14:40:37 +0100

    Created Login and Signup components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes,Route} from "react-router-dom"
 import './App.css';
 import Home from './pages/home/Home';
+import Login from './pages/authentication/Login/Login';
+import Signup from './pages/authentication/Signup/Signup';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={ <Home/> }/>
+      <Route path='/'  element={ <Home/> }/>
+      <Route path='/login'  element={ <Login />} />
+      <Route path='/signup'  element={ <Signup />} />
     </Routes>
   </BrowserRouter>
   );
