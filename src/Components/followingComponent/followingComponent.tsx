d4d5dde9a73a47858625a706cc1cc5e2ed47fa5a
ambiti0n <fabulousfresh010@gmail.com>
2022-02-24 22:37:12 +0100

    changePassword done

--- src/Components/followingComponent/followingComponent.tsx
+++ src/Components/followingComponent/followingComponent.tsx
@@ -1,34 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Following/following";
 import { PersonPlusFill } from "react-bootstrap-icons";
 
 function FollowingComponent(props:any) {
-//   const [following, setFollowing] = useState([]);
 
-    // useEffect(() => {
-    //   const getFollowing = async () => {
-    //     try {
-    //       const res = await axios(
-    //         `http://localhost:3000/api/follow/?pageNo=1&pageSize=20`,
-    //         {
-    //           headers: {
-    //             Authorization:
-    //               "Bearer " +
-    //               "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsImlhdCI6MTY0NTU0OTM2NSwiZXhwIjoxNjQ1NTY3MzY1fQ.VsKioB8rYVDgqbE-1w-vpvLu6RvA9eYW-uSnT-G9VAU", //the token is a variable which holds the token
-    //           },
-    //         }
-    //       );
-    //       setFollowing(res.data.data);
-    //     //   console.log(following)
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
-    //   };
-    //   getFollowing();
-    // }, []);
 
 
   return (
@@ -37,7 +14,7 @@ function FollowingComponent(props:any) {
         <div className="header">
           <div className="img-component">
             <img
-              src="https://images.unsplash.com/photo-1607758164193-19539498ddf4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=758&q=80"
+              src={props.profilePic}
               alt=""
               className="follow-img"
             />
@@ -51,8 +28,8 @@ function FollowingComponent(props:any) {
           </Button>
         </div>
         <div>
-          <p className="description">
-             lorem ipsium lorem ipsium lorem ipsium lorem ipsium lorem
+          <p className="biodata">
+          {props.bioData}
           </p>
         </div>
       </article>
