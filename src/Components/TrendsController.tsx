b45651e3ce2dd1b320fb44f2a804f35b53c91090
adebayoakingbade <adebayo.akingbade@decagon.dev>
2022-03-03 02:08:50 +0100

    trending_follow

--- src/Components/TrendsController.tsx
+++ src/Components/TrendsController.tsx
@@ -1,19 +1,32 @@
 import styles from "../styles/Tweeting_style/TweetController.module.css";
+// import { followingContext } from "../Components/FollowingProvider";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import Nav from "./NavBar/Nav";
 import { Link, useParams } from "react-router-dom";
 import { UserContext } from "../hooks/useContext";
 import { BASE_URL } from "../constants/contants";
+// import Trending_Follow from "./Trending_Follow";
+// import Tweet from "./Tweet/Tweet";
+// import { Navbar } from "reactstrap";
+import Trending_Follow from "./Trending_Follow";
+import { BeatLoader } from "react-spinners";
 
 function TrendsController() {
+  
+
   const userToken: any = useContext(UserContext);
   const token = userToken.token;
   const params: any = useParams();
 
+  const [trends, setTrends] = useState<any>([]);
   const [hashtags, setHashtags] = useState<any>([]);
+  const [follow, setFollow] = useState<any[]>([]);
 
+  const urll = `${BASE_URL}api/trends`;
   const url = `${BASE_URL}api/trends/hashtag?hashtag=%23${params.id}&pageNo=1&pageSize=5`;
+  const uri = `${BASE_URL}api/follow/suggest/?pageNo=2&pageSize=5`;
+
 
   useEffect(() => {
     axios
@@ -29,14 +42,61 @@ function TrendsController() {
       .catch((err) => {
         console.log(err);
       });
+      axios
+      .get(uri, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setFollow(res.data["suggested-connection"].suggestedConnection);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      axios
+      .get(urll, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setTrends(res.data.data.trending);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
+  const handleFollow = (userId: string) => {
+    axios
+      .post(
+        `${BASE_URL}api/follow`,
+        { userId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        const newFollow = follow.filter(item => item._id !== userId)
+        setFollow(newFollow)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
       <Nav />
-      
+      {/* <Trending_Follow /> */}
       <div className={styles["tweet-wrapper"]}>
-        
+
         <div className={styles["tweet-body"]}>
           {hashtags?.length
             ? hashtags?.map((tag: any) => (
@@ -69,7 +129,7 @@ function TrendsController() {
                         src={tag.tweetImage}
                         alt=""
                       /> : ""}
-    
+
                       <div className="row">
                         <div className="col-7"></div>
                         <div className="col-5">
@@ -126,50 +186,53 @@ function TrendsController() {
               ))
             : ""}
         </div>
+        
+        <div className={styles["trending-n-follow"]}>
+        <div className={styles.trending}>
+          <h3>trends for you</h3>
+          <div className={styles.underline}></div>
+          {Object.keys(trends).length ? (
+            Object.keys(trends).map((trend: string) => (
+              <div key={trend} className={styles["trending-content"]}>
+                <Link to={`/trends/${trend.replace(/#/g, "")}`}>
+                  {trend}
+                  <span>{trends[trend].length} Tweets</span>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div className="d-flex justify-content-center my-3 h-100">
+              <BeatLoader color="#2F80ED" />
+            </div>
+          )}
+        </div>
 
         <div className={styles["who-to-follow"]}>
           <h3>Who to follow</h3>
           <div className={styles.underline}></div>
           <div className={styles["suggest-container"]}>
-            <div className={styles["suggest-content"]}>
-              <div className={styles["suggest-user"]}>
-                <img src="https://www.findjerry.tech/static/media/jerry2.a509e534aa3f11de93e1.png" />
-                <h3>
-                  Jerry Idoko Agada fdfdfdfdfdf dfdf
-                  <span>230k followers</span>
-                </h3>
-                <button>
-                  <span></span>follow
-                </button>
+            {follow.length ? (
+              follow.map((item) => (
+                <div className={styles["suggest-content"]}>
+                  <div className={styles["suggest-user"]}>
+                    <img src={item.profilePic} alt="" />
+                    <h3>{`${item.firstName} ${item.lastName}`}</h3>
+                    <button onClick={() => handleFollow(item._id)}>
+                      <span></span>follow
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="d-flex justify-content-center my-3">
+                <BeatLoader color="#2F80ED" />
               </div>
-              <Link to="">
-                Photographer and filmmake bsed in Nigeria Nollywoodfilmmake bsed
-                in Nigeria Nollywood
-              </Link>
-              <img src="https://previews.123rf.com/images/dagadu/dagadu1008/dagadu100800015/7511808-.jpg?fj=1" />
+            )}
 
-              <div className={styles.underline}></div>
-            </div>
-            <div className={styles["suggest-content"]}>
-              <div className={styles["suggest-user"]}>
-                <img src="https://www.findjerry.tech/static/media/jerry2.a509e534aa3f11de93e1.png" />
-                <h3>
-                  Softtechy Developer
-                  <span>120k followers</span>
-                </h3>
-                <button>
-                  <span></span>follow
-                </button>
-              </div>
-              <Link to="">
-                Photographer and filmmake bsed in Nigeria Nollywoodfilmmake bsed
-                in Nigeria Nollywood
-              </Link>
-              <img src="https://previews.123rf.com/images/dagadu/dagadu1008/dagadu100800015/7511808-.jpg?fj=1" />
-            </div>
+            <div className={styles.underline}></div>
           </div>
-          {/* End of sugestion container box */}
         </div>
+      </div>
       </div>
     </>
   );
