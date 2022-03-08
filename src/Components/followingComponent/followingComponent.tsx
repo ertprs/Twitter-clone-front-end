14c419b681926a14b2e66c2ea93400f2a9554a78
ambiti0n <fabulousfresh010@gmail.com>
2022-03-08 03:38:58 +0100

    conflict resolution

--- src/Components/followingComponent/followingComponent.tsx
+++ src/Components/followingComponent/followingComponent.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../Following/following";
+import "../Follower/follower";
 import { PersonPlusFill } from "react-bootstrap-icons";
 import axios from "axios";
 import { AuthContext } from "../../context/Auth.context";
